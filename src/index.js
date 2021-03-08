475ca79f5a0e2f8e28a69de9e72c79b9abe14b53
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2021-03-07 19:06:29 -0700

    Hero created and background pattern added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import userIcon from './assets/user-icon.png';
+import background from './assets/background.png';
+
 import './styles/styles.css';
+
+const userContainerTemplate = `
+  <figure class="Image-container">
+    <img src="${userIcon}" alt="">
+  </figure>
+`;
+
+window.addEventListener('load', insertTemplates);
+
+function insertTemplates() {
+  const hero = document.querySelector('.Hero-container');
+  const backgroundPatternElement = document.querySelector('.Background-pattern');
+  const imgElement = document.querySelector('#pattern');
+  const userContainer = document.createElement('div');
+  userContainer.classList.add('User-container');
+  userContainer.innerHTML = userContainerTemplate;
+  hero.append(userContainer);
+
+  imgElement.src = background;
+};
